af22eb7b838fdc1211a9131ba17d7e52c442484e
vintx64 <vintx64@gmail.com>
2023-08-17 10:48:35 +0300

    create ExpensesViewBody

---
+++ lib/features/home/presentation/views/widgets/expenses_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpensesViewBody extends StatelessWidget {
+  const ExpensesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
