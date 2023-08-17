084c3e0f37e082f64dae6c084c8e214c43445b09
vintx64 <vintx64@gmail.com>
2023-08-17 13:31:37 +0300

    edit primary color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFFFFFF);
+const kPrimaryColor = Color(0x00ffffff);
 const kLatoRegular = 'Lato-Regular';
 const kLatoBold = 'Lato-Bold';
