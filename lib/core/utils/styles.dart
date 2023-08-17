84abe18e54ee07db361a29f74fa5b25bcbfca6b0
vintx64 <vintx64@gmail.com>
2023-08-17 14:15:11 +0300

    Add style text to app bar

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_replicate_card_transaction/constants.dart';
+
+abstract class Styles {
+  static const appBarTxtStyle =
+      TextStyle(fontFamily: kLatoBold, color: Colors.black);
+}
--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_replicate_card_transaction/constants.dart';
 
+import '../../../../../core/utils/styles.dart';
+
 PreferredSizeWidget customAppBar({required String title}) {
   return AppBar(
     backgroundColor: kPrimaryColor,
     elevation: 0,
-    title: Text(title),
+    title: Text(title, style: Styles.appBarTxtStyle),
   );
 }
