1448c4b587898c24bd6d3be581f017f525a699b1
vintx64 <vintx64@gmail.com>
2023-08-18 16:59:55 +0300

    add text style to item tab bar

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -3,5 +3,7 @@ import 'package:flutter_replicate_card_transaction/constants.dart';
 
 abstract class Styles {
   static const appBarTxtStyle =
-      TextStyle(fontFamily: kLatoBold, color: Colors.black);
+      TextStyle(fontFamily: kLatoBold, color: Colors.black, fontSize: 24);
+  static const tabBarItemsStyle =
+      TextStyle(fontFamily: kLatoRegular, color: Colors.black, fontSize: 18);
 }
