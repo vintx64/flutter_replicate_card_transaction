8555b705364b4c681355d5157e422b245fa25764
vintx64 <vintx64@gmail.com>
2023-08-18 17:00:32 +0300

    modify app bar

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -5,6 +5,12 @@ import '../../../../../core/utils/styles.dart';
 
 PreferredSizeWidget customAppBar({required String title}) {
   return AppBar(
+    centerTitle: true,
+    leading: const Icon(
+      Icons.arrow_back_ios,
+      color: Colors.black,
+      size: 30,
+    ),
     backgroundColor: kPrimaryColor,
     elevation: 0,
     title: Text(title, style: Styles.appBarTxtStyle),
