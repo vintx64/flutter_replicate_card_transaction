473fc0d1e0579e5ae8569fdf6e176b9c3b6f2791
vintx64 <vintx64@gmail.com>
2023-08-27 17:32:25 +0300

    changing status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_replicate_card_transaction/constants.dart';
 import 'package:flutter_replicate_card_transaction/features/home/presentation/views/expenses_view.dart';
 
 void main() {
@@ -10,6 +12,9 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: kPrimaryColor, // Set your desired app bar color here
+    ));
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: ExpensesView(),
