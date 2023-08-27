a039650d41905237e10501134b0abbd24679a94e
vintx64 <vintx64@gmail.com>
2023-08-27 15:40:35 +0300

    adding animations to pie chart

--- lib/features/home/presentation/views/widgets/custom_pie_chart.dart
+++ lib/features/home/presentation/views/widgets/custom_pie_chart.dart
@@ -1,91 +1,3 @@
-// import 'dart:math';
-
-// import 'package:flutter/material.dart';
-
-// class MockData {
-//   final String expenseName;
-//   final double value;
-//   final int transactions;
-//   final double percentage;
-//   final Color color;
-
-//   MockData(this.expenseName, this.value, this.transactions, this.percentage,
-//       this.color);
-// }
-
-// class CustomPieChart extends StatelessWidget {
-//   CustomPieChart({super.key});
-//   final List<MockData> mockDataList = [
-//     MockData('Shopping', 3500, 73, 0.5, const Color.fromRGBO(192, 192, 222, 1)),
-//     MockData(
-//         'Wellness', 1750, 73, 0.25, const Color.fromRGBO(160, 210, 193, 1)),
-//     MockData(
-//         'Transport', 875, 73, 0.125, const Color.fromRGBO(255, 176, 182, 1)),
-//     MockData('Bars & Resturants', 437.50, 73, 0.0625,
-//         const Color.fromRGBO(255, 194, 165, 1)),
-//     MockData('Subscriptions', 4186, 73, 0.185,
-//         const Color.fromRGBO(142, 155, 197, 1)),
-//   ];
-//   @override
-//   Widget build(BuildContext context) {
-//     return PieChartWidget(mockDataList);
-//   }
-// }
-
-// class PieChartWidget extends StatefulWidget {
-//   final List<MockData> mockDataList;
-
-//   const PieChartWidget(this.mockDataList, {super.key});
-
-//   @override
-//   State<PieChartWidget> createState() => _PieChartWidgetState();
-// }
-
-// class _PieChartWidgetState extends State<PieChartWidget> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return CustomPaint(
-//       painter: PieChartPainter(widget.mockDataList, 6),
-//       child: Container(),
-//     );
-//   }
-// }
-
-// class PieChartPainter extends CustomPainter {
-//   final List<MockData> mockDataList;
-//   final double gapSize;
-//   PieChartPainter(
-//     this.mockDataList,
-//     this.gapSize,
-//   );
-//   @override
-//   void paint(Canvas canvas, Size size) {
-//     final c = Offset(size.width / 2.0, size.height / 2.0);
-//     final raidus = size.width * 0.9;
-//     final rect = Rect.fromCenter(center: c, width: raidus, height: raidus);
-//     var totalPercentage =
-//         mockDataList.fold(0.0, (sum, data) => sum + data.percentage);
-//     var totalGapSize = gapSize * mockDataList.length;
-//     var totalAngle = 360.0 - totalGapSize;
-//     var gapAngle = gapSize * pi / 180.0;
-//     var startAngle = 0.0;
-
-//     for (var di in mockDataList) {
-//       var normalizedPercentage = di.percentage / totalPercentage;
-//       var sweepAngle = normalizedPercentage * totalAngle * pi / 180.0;
-//       final paint = Paint()
-//         ..strokeCap = StrokeCap.round
-//         ..style = PaintingStyle.stroke
-//         ..strokeWidth = 10
-//         ..color = di.color;
-//       canvas.drawArc(rect, startAngle, sweepAngle, false, paint);
-//       startAngle += sweepAngle + gapAngle;
-//     }
-//   }
-
-//   @override
-//   bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
-// }
 import 'dart:math';
 import 'package:flutter/material.dart';
 
@@ -100,39 +12,95 @@ class ExpenseData {
 class ExpenseRepository {
   List<ExpenseData> getExpenses() {
     return [
-      ExpenseData('Shopping', 0.5, Color.fromRGBO(192, 192, 222, 1)),
-      ExpenseData('Wellness', 0.25, Color.fromRGBO(160, 210, 193, 1)),
-      ExpenseData('Transport', 0.125, Color.fromRGBO(255, 176, 182, 1)),
-      ExpenseData('Subscriptions', 0.185, Color.fromRGBO(142, 155, 197, 1)),
+      ExpenseData('Shopping', 0.5, const Color.fromRGBO(192, 192, 222, 1)),
+      ExpenseData('Wellness', 0.25, const Color.fromRGBO(160, 210, 193, 1)),
+      ExpenseData('Transport', 0.125, const Color.fromRGBO(255, 176, 182, 1)),
       ExpenseData(
-          'Bars & Restaurants', 0.0625, Color.fromRGBO(255, 194, 165, 1)),
+          'Subscriptions', 0.185, const Color.fromRGBO(142, 155, 197, 1)),
+      ExpenseData(
+          'Bars & Restaurants', 0.0625, const Color.fromRGBO(255, 194, 165, 1)),
     ];
   }
 }
 
-class CustomPieChart extends StatelessWidget {
-  final ExpenseRepository repository = ExpenseRepository();
+class CustomPieChart extends StatefulWidget {
+  @override
+  _CustomPieChartState createState() => _CustomPieChartState();
+}
 
-  CustomPieChart({super.key});
+class _CustomPieChartState extends State<CustomPieChart>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _rotationAnimation;
+  late Animation<double> _textOpacityAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    _rotationAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _textOpacityAnimation = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Interval(0.6, 1.0),
+      ),
+    );
+
+    _animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final mockDataList = repository.getExpenses();
-    return PieChartWidget(mockDataList, gapSize: 6);
+    final mockDataList = ExpenseRepository().getExpenses();
+    return AnimatedBuilder(
+      animation: _animationController,
+      builder: (context, child) {
+        return PieChartWidget(
+          expenseDataList: mockDataList,
+          rotation: _rotationAnimation.value * 360.0,
+          textOpacity: _textOpacityAnimation.value,
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 }
 
 class PieChartWidget extends StatelessWidget {
   final List<ExpenseData> expenseDataList;
-  final double gapSize;
+  final double rotation;
+  final double textOpacity;
 
-  const PieChartWidget(this.expenseDataList,
-      {super.key, required this.gapSize});
+  const PieChartWidget({
+    required this.expenseDataList,
+    required this.rotation,
+    required this.textOpacity,
+  });
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: PieChartPainter(expenseDataList, gapSize),
+      painter: PieChartPainter(
+        expenseDataList: expenseDataList,
+        rotation: rotation,
+        textOpacity: textOpacity,
+      ),
       child: Container(),
     );
   }
@@ -140,9 +108,15 @@ class PieChartWidget extends StatelessWidget {
 
 class PieChartPainter extends CustomPainter {
   final List<ExpenseData> expenseDataList;
-  final double gapSize;
+  final double rotation;
+  final double textOpacity;
+  final double gapSize = 6; // Adjust the gap size between arcs
 
-  PieChartPainter(this.expenseDataList, this.gapSize);
+  PieChartPainter({
+    required this.expenseDataList,
+    required this.rotation,
+    required this.textOpacity,
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -152,34 +126,65 @@ class PieChartPainter extends CustomPainter {
 
     var totalPercentage =
         expenseDataList.fold(0.0, (sum, data) => sum + data.percentage);
-    var totalGapSize = gapSize * expenseDataList.length;
-    var totalAngle = 360.0 - totalGapSize;
+    var totalAngle = 360.0;
+    var gapCount = expenseDataList.length;
+    var totalGapSize = gapCount * gapSize;
+    var remainingAngle = totalAngle - totalGapSize;
     var gapAngle = gapSize * pi / 180.0;
-    var startAngle = -pi / 2.0;
+
+    var startAngle = (-pi / 2.0) + (rotation * pi / 180.0);
 
     for (var data in expenseDataList) {
       var normalizedPercentage = data.percentage / totalPercentage;
-      var sweepAngle = normalizedPercentage * totalAngle * pi / 180.0;
+      var sweepAngle = normalizedPercentage * remainingAngle * pi / 180.0;
+
       final paint = Paint()
         ..strokeCap = StrokeCap.round
         ..style = PaintingStyle.stroke
         ..strokeWidth = 8
         ..color = data.color;
+
       canvas.drawArc(rect, startAngle, sweepAngle, false, paint);
 
       startAngle += sweepAngle + gapAngle;
     }
-    final textPainter = TextPainter(
+
+    final textPainter1 = TextPainter(
       text: TextSpan(
-        text: 'Center Text', // Replace with your desired text
-        style: TextStyle(fontSize: 16, color: Colors.black),
+        text: 'Total Spent',
+        style: TextStyle(
+          fontSize: 22,
+          color: Colors.black.withOpacity(0.2 * textOpacity),
+        ),
       ),
       textDirection: TextDirection.ltr,
     );
-    textPainter.layout(minWidth: 0, maxWidth: size.width);
-    final textX = center.dx - textPainter.width / 2;
-    final textY = center.dy - textPainter.height / 2;
-    textPainter.paint(canvas, Offset(textX, textY));
+    textPainter1.layout();
+
+    final textPainter2 = TextPainter(
+      text: TextSpan(
+        text: '\$9,000.00',
+        style: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+          color: Colors.black.withOpacity(textOpacity),
+        ),
+      ),
+      textDirection: TextDirection.ltr,
+    );
+    textPainter2.layout();
+
+    final text1Offset = Offset(
+      center.dx - textPainter1.width / 2,
+      center.dy - textPainter1.height - 8,
+    );
+    textPainter1.paint(canvas, text1Offset);
+
+    final text2Offset = Offset(
+      center.dx - textPainter2.width / 2,
+      center.dy - 8,
+    );
+    textPainter2.paint(canvas, text2Offset);
   }
 
   @override
